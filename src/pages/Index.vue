2e410f55886b6e0974cd95dd5d9f3da822f2bf05
Thomas Abbondi <digital.crystalware@gmail.com>
2021-09-03 18:29:51 +0200

    Installed vue-leaflet

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page class="flex items-stretch">
+    <l-map
+      ref="map"
+      :zoom="zoom"
+      :center="center"
+      style="height: auto;"
     >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+    </l-map>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  components: {
+    LMap, LTileLayer
+  },
+  name: 'PageIndex',
+  data() {
+    return {
+      zoom: 10,
+      center: [47.41322, 10.219482]
+    };
+  },
+});
 </script>
