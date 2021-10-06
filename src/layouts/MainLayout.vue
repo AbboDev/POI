f04c51237242416831f06b938b7996b945ea8106
Thomas Abbondi <digital.crystalware@gmail.com>
2021-10-06 20:55:21 +0200

    Create base OV2 files import

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,7 +24,7 @@
       show-if-above
       bordered
     >
-      <q-list separator>
+      <q-list>
         <q-item-label header>
           Points
         </q-item-label>
@@ -35,6 +35,8 @@
           v-bind="item"
           :coordinates="item.center"
         />
+
+        <q-separator />
 
         <q-item
           clickable
@@ -47,6 +49,29 @@
 
           <q-item-section>
             <q-item-label>Add point</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section>
+            <q-file
+              clearable
+              borderless
+              v-model="file"
+              label="Import points"
+            >
+              <template #before>
+                <q-icon name="share_location" />
+              </template>
+              <template #after>
+                <q-btn
+                  round
+                  color="primary"
+                  icon="send"
+                  @click="importPoints"
+                />
+              </template>
+            </q-file>
           </q-item-section>
         </q-item>
       </q-list>
@@ -63,7 +88,7 @@ import { uid } from 'quasar';
 import EssentialLink from 'components/EssentialLink.vue';
 import { defineComponent, ref } from 'vue';
 
-const linksList = [];
+const Buffer = require('buffer/').Buffer;
 
 export default defineComponent({
   name: 'MainLayout',
@@ -76,7 +101,7 @@ export default defineComponent({
     const leftDrawerOpen = ref(false);
 
     return {
-      essentialLinks: linksList,
+      file: ref(null),
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value;
@@ -85,11 +110,124 @@ export default defineComponent({
   },
   methods: {
     addPoint() {
-      const id = uid();
       this.$store.dispatch('map/push', {
-        id,
+        id: uid(),
         title: 'Nuovo punto',
       });
+    },
+    importPoints() {
+      const reader = new FileReader();
+
+      const ascii2utf8 = function(buffer) {
+        let c;
+        let i;
+        let j;
+
+        // New buffer with utf8 string:
+        const utf8 = Buffer.alloc(buffer.length * 2);
+
+        // Convert:
+        j = 0;
+        for (i = 0; i < buffer.length; i++) {
+          c = buffer.readUInt8(i, 1);
+
+          if (c < 0x80) {
+            utf8.writeUInt8(buffer.readUInt8(i, 1), j += 1);
+          } else if (c < 0xc0) {
+            utf8.writeUInt8(0xc2, j += 1);
+            utf8.writeUInt8(buffer.readUInt8(i, 1), j += 1);
+          } else {
+            utf8.writeUInt8(0xc3, j += 1);
+            utf8.writeUInt8(buffer.readUInt8(i, 1) - 0x40, j += 1);
+          }
+        }
+
+        return utf8.toString('utf8', 0, j);
+      };
+
+      reader.onload = (event) => {
+        const ov2 = Buffer.from(event.target.result);
+        const records = [];
+
+        let type;
+        let rlength;
+
+        let record = 0;
+        let i = 0;
+
+        record = 0;
+        i = 0;
+
+        try {
+          do {
+            type = ov2.readUInt8(i + 0);
+            rlength = ov2.readUInt32LE(i + 1);
+
+            switch (type) {
+            // Deleted record:
+            case 0:
+              throw new Error(`The type "${type}" is not yet decoded!`);
+
+            // Skipper Record:
+            case 1:
+              throw new Error(`The type "${type}" is not yet decoded!`);
+              // records[record] = {
+              //   record,
+              //   type,
+              //   bytes: rlength,
+              //   λwest: ov2.readInt32LE(i + 5) / 100000,
+              //   φsouth: ov2.readInt32LE(i + 9) / 100000,
+              //   λeast: ov2.readInt32LE(i + 13) / 100000,
+              //   φnorth: ov2.readInt32LE(i + 17) / 100000,
+              // };
+
+              // i += 21;
+              // record += 1;
+              // break;
+
+            // Simple POI Record:
+            case 2:
+              records[record] = {
+                record,
+                type,
+                length: rlength,
+                lng: ov2.readInt32LE(i + 5) / 100000,
+                lat: ov2.readInt32LE(i + 9) / 100000,
+                info: ascii2utf8(ov2.slice(i + 13, i + (rlength)))
+                  .replace(/\0/g, ''),
+              };
+
+              if (records[record].lng && records[record].lat) {
+                if (!records[record].info) {
+                  records[record].info = 'Nuovo punto';
+                }
+                this.$store.dispatch('map/push', {
+                  id: uid(),
+                  title: records[record].info,
+                  center: [records[record].lat, records[record].lng]
+                });
+              } else {
+                throw new Error(`The record "${record}" misses properties!`);
+              }
+
+              i += ov2.readUInt32LE(i + 1);
+              record += 1;
+              break;
+
+            // Extended POI Record:
+            case 3:
+              throw new Error(`The type "${type}" is not yet decoded!`);
+
+            default:
+              throw new Error(`The type "${type}" is unknown!`);
+            }
+          } while (i < ov2.length);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      reader.readAsArrayBuffer(this.file);
     }
   }
 });
